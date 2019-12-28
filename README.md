3da6c046da53a6633b9e729794e432481edec1d1
xiaonaiquan <ryan_naiquan@163.com>
2019-12-28 14:59:27 +0800

    提交hyper_function

--- README.md
+++ README.md
@@ -4,10 +4,16 @@
 2. post_demo post任务的demo，执行一次
 
 ### C++11消息工厂
-1. message_demo消息工厂的demo   :  g++ main.cpp -o main
+
+message_demo消息工厂的demo   :  g++ main.cpp -o main
 
 ### c++pimpl模式
 
 ### 利用策略和工厂设计模式消除过多的if-else语句
 
-	代码是java的伪代码
+代码是java的伪代码，增加了C++的实现
+
+### 增加了**[hyper_function.hpp](https://gist.github.com/lingol/ed9feab92da9e341487855084411df4e)**
+
+hyper_function.hpp是腾讯大佬的优化版本，作用是类似于std :: function的util，具有接受继承功能的功能。main.cpp是使用案例，也是微信团队优化的案例（地址：https://mp.weixin.qq.com/s/-wgBhE11xEXDS7Hqgq3FjA），避免基类被模板化。
+
---
+++ hyper_function/main.cpp
@@ -0,0 +1,102 @@
+﻿#include <iostream>
+#include <functional>
+#include <vector>
+#include "hyper_function.h"
+
+//原始版本
+//template <typename Request, typename Response>
+//class BaseCgi {
+//public:
+//	BaseCgi(Request request, std::function<void(Response &)> &callback) {
+//		_request = request;
+//		_callback = callback;
+//	}
+//
+//	void onRequest(std::vector<uint8_t> &outData) {
+//		_request.toData(outData);
+//	}
+//
+//	void onResponse(std::vector<uint8_t> &inData) {
+//		Response response;
+//		response.fromData(inData);
+//		callback(response);
+//	}
+//
+//public:
+//	Request _request;
+//	std::function<void(Response &)> _callback;
+//};
+//
+//class CgiA : public BaseCgi<RequestA, ResponseA> {
+//public:
+//	CgiA(RequestA &request, std::function<void(ResponseA &)> &callback) :
+//		BaseCgi(request, callback) {}
+//};
+
+
+//优化的版本
+class BaseRequest {
+public:
+	virtual void toData(std::vector<uint8_t> &outData) = 0;
+};
+
+class BaseResponse {
+public:
+	virtual void fromData(std::vector<uint8_t> &outData) = 0;
+};
+
+class BaseCgi {
+public:
+	template <typename Request, typename Response>
+	BaseCgi(Request &request, hyper_function<void(Response &)> callback) {
+		_request = new Request(request);
+		_response = new Response;
+		_callback = callback;
+	}
+
+	void onRequest(std::vector<uint8_t> &outData) {
+		_request->toData(outData);
+	}
+
+	void onResponse(std::vector<uint8_t> &inData) {
+		_response->fromData(inData);
+		_callback(*_response);
+	}
+
+public:
+	BaseRequest *_request;
+	BaseResponse *_response;
+	hyper_function<void(BaseResponse &)> _callback;
+};
+
+class RequestA : public BaseRequest 
+{ 
+public:
+	void toData(std::vector<uint8_t> &outData)
+	{
+
+	}
+};
+
+class ResponseA : public BaseResponse 
+{ 
+public:
+	void fromData(std::vector<uint8_t> &outData)
+	{
+
+	}
+};
+
+class CgiA : public BaseCgi {
+public:
+	CgiA(RequestA &request, hyper_function<void(ResponseA &)> &callback) :
+		BaseCgi(request, callback) //子类function对象给父类
+	{
+	}
+};
+
+int main()
+{
+	system("PAUSE");
+	return 0;
+}
